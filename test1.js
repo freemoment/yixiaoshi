0c084577e24867b5a1cffd0b2e64eac0e22f5eee
spring77 <alibaba.springlee@gmail.com>
2014-08-27 23:46:34 +0800

    共伞count

--- test1.js
+++ test1.js
@@ -15,3 +15,20 @@ Parse.Cloud.define("InOneUmber", function(request, response) {
     }
   });
 });
+
+
+// is anyone contribution mode2 :共伞的数量
+Parse.Cloud.define("InOneUmberCount", function(request, response) {
+  query = new Parse.Query("UmbrellaContribution");
+  query.equalTo("mode", request.params.mode);
+  query.count({
+    success: function(count) {
+     
+        response.success();
+
+    },
+    error: function(error) {
+      response.error("Error " + error.code + " : " + error.message + " when query guys InOneUmberCount.");
+    }
+  });
+});
