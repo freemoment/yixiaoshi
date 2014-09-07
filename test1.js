bfa92d2604c24e8befb0ce5620ddc661f9746eb1
spring77 <alibaba.springlee@gmail.com>
2014-09-07 17:18:27 +0800

    1. 正在共伞数量 2. 取有伞&&顺路&&是异性的用户 （有待review）

--- test1.js
+++ test1.js
@@ -1,26 +1,9 @@
 
-
-// is anyone contribution mode2 :共伞 
-Parse.Cloud.define("InOneUmber", function(request, response) {
-  query = new Parse.Query("UmbrellaContribution");
-  query.equalTo("mode", request.params.mode);
-  query.find({
-    success: function(result) {
-     
-        response.success();
-
-    },
-    error: function(error) {
-      response.error("Error " + error.code + " : " + error.message + " when query guys InOneUmber.");
-    }
-  });
-});
-
-
-// is anyone contribution mode2 :共伞的数量
+// count of contribution  :取正在共伞的数量
 Parse.Cloud.define("InOneUmberCount", function(request, response) {
-  query = new Parse.Query("UmbrellaContribution");
-  query.equalTo("mode", request.params.mode);
+  query = new Parse.Query("Relationship");
+  query.equalTo("isActive", "true");
+  query.equalTo("status", 3);
   query.count({
     success: function(count) {
      
@@ -32,3 +15,53 @@ Parse.Cloud.define("InOneUmberCount", function(request, response) {
     }
   });
 });
+
+
+//取有伞&&顺路&&是异性的用户
+Parse.Cloud.define("getMatchUser", function(request, response) {
+                   //当前用户id
+                   var userId = request.params.userId;
+                   var userQuery = new Parse.Query(Parse.User);
+
+                   userQuery.get(userId, {
+                                 success: function(user) {
+
+                                 //获取有伞状态
+								 var innerQueryForUmbrellaContribution = new Parse.Query('UmbrellaContribution');
+								 innerQueryForUmbrellaContribution.equalTo("isContributor",true);
+                                 //获取当前用户 终点 geo
+                                 var innerQueryForRoute = new Parse.Query('Route');
+                                 innerQueryForRoute.equalTo("user",user);
+                                 // 获取顺路的用户
+                                 var innerQueryForRouteSameEnd new Parse.Query('Route');
+                                 innerQueryForRouteSameEnd.matchesQuery("end",innerQueryForRoute);
+                                 //获取异性
+                                 var gender = user.get("gender");
+                                 if (gender == 0) {
+                                 	gender =1;
+                                 }else{
+                                 	gender =0;
+                                 }
+                                 var innerQueryForUser new Parse.Query('User');
+                                 innerQueryForUser.equalTo("gender",gender);
+                                 innerQueryForUser.matchesQuery("objectId",innerQueryForUser.matchesQuery("objectId",innerQueryForUmbrellaContribution));
+
+                                 query.find({
+                                             success: function(users) {
+                                                response.success();
+                                             },
+                                             error: function(error) {
+                                                response.error(error);
+                                             }
+                                 });
+                                 },
+                                 error: function(user, error) {
+                                    response.error(error);
+                                 }
+                   });
+});
+
+
+
+
+// 发一个请求
